1a63ef3502346401f181cedad12ec0964d0e1a71
Jeremy Gunawan <jg.jeremy.gunawan@gmail.com>
2025-03-23 13:59:55 +0700

    create signin api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt  from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, password, email } = req.body;
@@ -9,6 +11,27 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.status(201).json('User Created Successfully!');
+    } catch (error) {
+        next(error);
+    }
+
+};
+
+export const signin = async (req, res, next) => {
+    const { password, email } = req.body;
+    const hashPass = bcrypt.hashSync(password, 10);
+
+    try {
+        const validUser = await User.findOne({email: email});
+        if(!validUser) return next(errorHandler(404, 'User Not Found!'));
+        
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+
+        const {password: pass, ...rest} = validUser._doc;
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
     } catch (error) {
         next(error);
     }
